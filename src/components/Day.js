fc8e9d3487b8a038b226ab91b509313a281b74a5
Steven Del Rosario <stevendelro@pm.me>
2019-09-04 13:04:05 -0700

    Displaing day of the week. Formatted temp data.

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
 const Day = props => {
+  const timestamp = props.time
+  const date = new Date()
+  date.setTime(timestamp*1000)
+
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+
   return (
     <div className='day'>
-      <p>Day: {props.time}</p>
+      <p>Day: {weekdays[date.getDay()]} </p>
       <p className='day--icon'>Icon: {props.icon}</p>
-      <p className='day--temp-hi'>High: {props.tempHi}</p>
-      <p className='day--temp-lo'>Low: {props.tempLo}</p>
+      <p className='day--temp-hi'>{`High: ${Math.round(props.tempHi)}°F`}</p>
+      <p className='day--temp-lo'>{`Low: ${Math.round(props.tempLo)}°F`}</p>
+      <hr />
     </div>
   )
 }
